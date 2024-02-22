9543f3bc3e78c49b3ccb3c4514239e1083d6fb64
none <loja.bobaev2000@yandex.ru>
2024-02-22 10:23:00 +0300

    add: базовые страницы

---
+++ frontend/src/app/admin/layout.tsx
@@ -0,0 +1,10 @@
+import { ILayout } from "@/interfaces/ILayout";
+import st from "./page.module.css";
+
+export default function LayoutAdmin({children}: ILayout) {
+    return (
+        <div className={st.page}>
+            
+        </div>
+    )
+}
---
+++ frontend/src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+import st from "./page.module.css";
+
+export default function Admin() {
+    return (
+        <div className={st.page}>
+            
+        </div>
+    )
+}
